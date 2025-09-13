5a0fe76b18cf0776b01bb1752cfc6e6a944c79e0
ArminJaemaa <ajaemaa@tlu.ee>
2025-09-13 15:23:48 +0300

    feat(counter): add buttons to increase and decrease count

--- 2025-09-11/my-app/src/components/Counter.tsx
+++ 2025-09-11/my-app/src/components/Counter.tsx
@@ -4,18 +4,34 @@ import "../App.css"
 function Counter() {
   const [count, setCount] = useState(0)
 
-  function IncreaseCounter(){
-     setCount(count => count + 1)
+  function IncreaseCounter(countButton: number) {
+    setCount(count => count + countButton)
   }
+
+  const positiveButtons = [100, 50, 25, 1]
+  const negativeButtons = [-1, -25, -50, -100]
 
   return (
     <>
-     
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={IncreaseCounter}>
-          count is {count}
-        </button>
+        {negativeButtons.map((amount, index) => (
+          <button
+            key={index}
+            onClick={() => IncreaseCounter(amount)}
+          >
+            {amount}
+          </button>
+        ))}
+        <button>count is {count}</button>
+        {positiveButtons.map((amount, index) => (
+          <button
+            key={index}
+            onClick={() => IncreaseCounter(amount)}
+          >
+            +{amount}
+          </button>
+        ))}
       </div>
     </>
   )
