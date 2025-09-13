425a359798d97e171982224c38150e98b034ce34
ArminJaemaa <ajaemaa@tlu.ee>
2025-09-13 17:06:50 +0300

    feat(ProfileCard): add name, hobbies and form

---
+++ 2025-09-11/my-app/src/components/ProfileCard.tsx
@@ -0,0 +1,37 @@
+import styles from "./ProfileCard.module.css"
+
+function ProfileCard() {
+  return (
+    <>
+      <div className={styles.card}>
+        <h1>Armin Jaemaa</h1>
+        <ul>
+          <strong>hobid:</strong>
+          <li>jõusaal</li>
+          <li>reisimine</li>
+          <li>arvutimängud</li>
+        </ul>
+        <form>
+          <div className={styles.field}>
+            <label htmlFor="email">email:</label>
+            <input
+              type="text"
+              id="email"
+              placeholder="email"
+            />
+          </div>
+          <div className={styles.field}>
+            <label htmlFor="txt">text:</label>
+            <input
+              type="text"
+              id="txt"
+            />
+          </div>
+        </form>
+        <button>nupp</button>
+      </div>
+    </>
+  )
+}
+
+export default ProfileCard
