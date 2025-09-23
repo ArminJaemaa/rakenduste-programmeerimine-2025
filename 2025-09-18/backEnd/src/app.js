44c8f54649d53f51b875f749ccf7fae066349531
ArminJaemaa <ajaemaa@tlu.ee>
2025-09-23 19:51:49 +0300

    chore: add console examples for testing

--- 2025-09-18/backEnd/src/app.js
+++ 2025-09-18/backEnd/src/app.js
@@ -3,10 +3,33 @@ const itemsRouter = require('./routes/itemRouter')
 
 const app = express()
 
+const kasutajad = [
+    { nimi: "Armin", vanus: 23 },
+    { nimi: "karo", vanus: 100 },
+]
+
 app.use(express.json())
 
 app.use('/greeting', (req, res) => {
     res.send("hello Armin!")
+    console.error("see on viga")
+    console.warn("see on hoiatus")
+    console.info("See on lihtsalt info.")
+    console.table(kasutajad)
+    console.time("loop");
+    for (let i = 0; i < 1000000; i++) { }
+    console.timeEnd("loop")
+    console.assert(1 === 2, "See väide on vale!")
+    console.assert(2 === 2, "Seda ei näidata.")
+    console.group("Kasutaja andmed")
+    console.log("Nimi: Armin")
+    console.log("Vanus: 23")
+    console.groupEnd()
+    console.count("loendur");
+    console.count("loendur");
+    console.count("loendur");
+
+
 })
 
 app.use('/items', itemsRouter)
