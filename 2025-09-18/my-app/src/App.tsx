8cd4a30178996d35fe89c594cc19ea5f145f0a3b
ArminJaemaa <ajaemaa@tlu.ee>
2025-09-23 20:47:22 +0300

    feat: add homepage info and add MUI appbar for navigation

--- 2025-09-18/my-app/src/App.tsx
+++ 2025-09-18/my-app/src/App.tsx
@@ -3,27 +3,58 @@ import { Routes, Route } from "react-router"
 import HomePage from "./pages/HomePage"
 import CounterPage from "./pages/CounterPage"
 import About from "./pages/About"
+import { AppBar, Toolbar, Typography, Button, Container } from "@mui/material"
+import { Link } from "react-router"
 
 function App() {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={<HomePage />}
-      />
-      <Route
-        path="counter-page"
-        element={<CounterPage />}
-      />
-      <Route
-        path="about"
-        element={<About />}
-      />
-      <Route
-        path="/*"
-        element={<div>Page Not Found</div>}
-      />
-    </Routes>
+    <>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6"></Typography>
+          <Button
+            color="inherit"
+            component={Link}
+            to="/"
+          >
+            Home
+          </Button>
+          <Button
+            color="inherit"
+            component={Link}
+            to="/about"
+          >
+            Minust
+          </Button>
+          <Button
+            color="inherit"
+            component={Link}
+            to="/counter-page"
+          >
+            Counter
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Container sx={{ mt: 4 }}></Container>
+      <Routes>
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+        <Route
+          path="counter-page"
+          element={<CounterPage />}
+        />
+        <Route
+          path="about"
+          element={<About />}
+        />
+        <Route
+          path="/*"
+          element={<div>Page Not Found</div>}
+        />
+      </Routes>
+    </>
   )
 }
 
--- 2025-09-18/my-app/src/pages/HomePage.tsx
+++ 2025-09-18/my-app/src/pages/HomePage.tsx
@@ -1,24 +1,33 @@
-import Button from "@mui/material/Button"
-import ButtonGroup from "@mui/material/ButtonGroup"
-import { Link } from "react-router"
+import { Typography } from "@mui/material"
 import Dice from "../components/Dice"
+
 function HomePage() {
   return (
     <>
       <Dice />
-      <ButtonGroup
-        orientation="vertical"
-        size="large"
-        variant="outlined"
-        aria-label="button group"
-      >
-        <Link to="/counter-page">
-          <Button>Counter</Button>
-        </Link>
-        <Link to="/about">
-          <Button>About</Button>
-        </Link>
-      </ButtonGroup>
+      <Typography>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in enim
+        luctus est viverra blandit at eu nunc. Donec velit justo, faucibus quis
+        imperdiet at, tincidunt vitae massa. In a urna quam. Proin eu neque ut
+        nisl maximus hendrerit at in augue. Nam pretium nunc vel viverra
+        vulputate. Aenean quam lorem, interdum at dapibus et, iaculis at enim.
+        Praesent euismod augue massa. Fusce non elit faucibus, elementum sapien
+        ut, dapibus quam. Aliquam sagittis laoreet nibh, non sollicitudin ante
+        gravida at. Ut nisi ex, accumsan id nisl eget, imperdiet vehicula
+        libero. Nunc non ipsum eget augue aliquet convallis. Quisque tristique
+        est ut gravida varius. Aliquam ut nunc mauris. Nullam ac luctus ligula.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam rutrum
+        dolor at velit tincidunt, et elementum lectus dapibus. Sed mattis diam
+        sit amet justo viverra, vel faucibus ipsum facilisis. Vivamus rutrum,
+        metus ac efficitur rhoncus, libero lacus interdum tortor, vitae commodo
+        sapien turpis a nunc. Aliquam erat volutpat. Praesent venenatis dolor ut
+        rhoncus condimentum. Aenean sit amet enim at tortor feugiat aliquam.
+        Curabitur at neque quam. Morbi finibus lacinia purus, porttitor egestas
+        odio commodo vel. Mauris ac justo vehicula, tempor elit non, accumsan
+        lorem. Pellentesque quam mauris, convallis iaculis ligula in, aliquam
+        tincidunt tellus. Donec varius sollicitudin dapibus. Donec sit amet
+        pharetra lorem, quis volutpat sem. Mauris laoreet placerat libero.
+      </Typography>
     </>
   )
 }
